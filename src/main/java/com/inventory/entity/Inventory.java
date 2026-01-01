e94611c4646d494375df738d5f47d914371dc5c9
Sulakshana Ranawake <sula.ranawake@gmail.com>
2026-01-02 01:20:39 +0530

    Inventory entity created

---
+++ src/main/java/com/inventory/entity/Inventory.java
@@ -0,0 +1,27 @@
+package com.inventory.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "inventory")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+}
